a92721314f70137deec77d2dbc0cb1948c70b13a
Yevhenii <ke1vin.kapustian@gmail.com>
2025-08-21 16:37:51 +0200

    Completed with OptionButton

---
+++ src/components/OptionButton/index.tsx
@@ -0,0 +1,19 @@
+type OptionButtonProps = {
+    option: string,
+    handleClick: () => void,
+    userAnswer: string | null,
+    rightAnswer: string
+}
+
+const OptionButton = ({option, handleClick, userAnswer, rightAnswer}:OptionButtonProps) => {
+    return(
+        <button className={
+                    userAnswer && option === rightAnswer ? "right" 
+                    : userAnswer && option === userAnswer ? "wrong" 
+                    : ""
+                } 
+                onClick={handleClick}>{option}</button>
+    )
+}
+
+export default OptionButton
