aa8cc9989aab4e9051eaf61af792e45e634ff6fe
Yevhenii <ke1vin.kapustian@gmail.com>
2025-08-21 23:49:01 +0200

    Added new changes

--- src/components/OptionButton/index.tsx
+++ src/components/OptionButton/index.tsx
@@ -1,20 +1,26 @@
 type OptionButtonProps = {
-    option: string,
-    handleClick: () => void,
-    userAnswer: string | null,
-    rightAnswer: string
+  option: string,
+  handleClick: () => void,
+  userAnswer: string | null,
+  rightAnswer: string
 }
 
-const OptionButton = ({option, handleClick, userAnswer, rightAnswer}:OptionButtonProps) => {
-    return(
-        <button data-testid="option-button" 
-                className={
-                    userAnswer && option === rightAnswer ? "right" 
-                    : userAnswer && option === userAnswer ? "wrong" 
-                    : ""
-                } 
-                onClick={handleClick}>{option}</button>
-    )
+const OptionButton = ({ option, handleClick, userAnswer, rightAnswer }: OptionButtonProps) => {
+    const isDisabled = userAnswer ? true : false;
+
+  return (
+    <button
+      data-testid="option-button"
+      onClick={() => !isDisabled && handleClick()}
+      disabled={isDisabled}
+      className={
+        userAnswer && option === rightAnswer ? "right" :
+        userAnswer && option === userAnswer ? "wrong" : ""
+      }
+    >
+      {option}
+    </button>
+  )
 }
 
-export default OptionButton
+export default OptionButton;
