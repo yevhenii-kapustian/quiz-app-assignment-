1db5650ec86a6e1d0eb363aca8286f88dd5dab12
Yevhenii <ke1vin.kapustian@gmail.com>
2025-08-23 15:29:54 +0200

    Added <main> in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header/>
-        {children}
+        <main className="px-5">
+          {children}
+        </main>
       </body>
     </html>
   );
